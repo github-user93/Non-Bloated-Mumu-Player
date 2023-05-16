7ddb6c9e4c567ce619800063d9106e304b3c3e85
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-05-16 10:05:11 +0300

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,31 @@
+color a
+cd C:\Users\%USERNAME%\Downloads
+curl https://a11.gdl.netease.com/MuMuInstaller_12.0.0.6_12beta-gw_all_1666787338.exe -o Mumu.exe
+mkdir AndroidLaunchers
+cd C:\Users\%USERNAME%\Downloads\AndroidLaunchers
+curl https://github.com/LawnchairLauncher/lawnchair/releases/download/v12.1.0-alpha.4/Lawnchair.12.1.0.Alpha.4.apk -o Lawnchair.12.1.0.Alpha.4.apk
+curl https://f-droid.org/repo/fr.neamar.kiss_206.apk -o fr.neamar.kiss_206.apk
+curl https://cloclo54.datacloudmail.ru/public/get/7YHNpnRnuzvjepP2FhB626HUvf1Bzo3GPk2re4drn6SR8ykwQhBmC4nonMqVxPAYdSVRkT/no/novalauncherprime55modt-androidoyunclub.apk -o novalauncherprime55modt-androidoyunclub.apk 
+curl https://github.com/MM2-0/Kvaesitso/releases/download/v1.24.2/de.mm20.launcher2.release_2023042300.apk -o de.mm20.launcher2.release_2023042300.apk
+curl http://download2278.mediafire.com/k05ja03gjfagjDQTR9ukITJVhufO7T1sgGy2Ona971lJAq20mWvHfWZ40pZII0pWWjL9ETQ3OUaAC4kbamuY_zo5gp_ajeKWuuBacVOYwFIA5Cxbfjlcn7DvWPg7IzAWe9wK-DAjRoPU29-y1HySZpibmBrvKGoOm-LvM69R6EviYQ/ozygcd568e1amct/Trebuchet+8.1.0.22+UNIVERSAL.apk -o Trebuchet8.1.0.22UNIVERSAL.apk 
+echo When the installer is done press Launch Please!!!
+echo When the installer is done press Launch Please!!!
+echo When the installer is done press Launch Please!!!
+C:\Users\%USERNAME%\Downloads\Mumu.exe
+@NetSh AdvFirewall Set AllProfiles State On
+
+:: Variables
+set RULE_NAME=TemporaryBlock
+set PROGRAM="C:\Program Files\MuMu9\emulator\nemu9\EmulatorShell\NemuPlayer.exe"
+
+netsh advfirewall firewall add rule name="BluestacksBlocker" dir=in action=block profile=any program="C:\Program Files\MuMu9\emulator\nemu9\EmulatorShell\NemuPlayer.exe"
+netsh advfirewall firewall add rule name="BluestacksBlocker" dir=out action=block profile=any program="C:\Program Files\MuMu9\emulator\nemu9\EmulatorShell\NemuPlayer.exe"
+C:\Program Files\MuMu9\emulator\nemu9\EmulatorShell\NemuPlayer.exe
+start "" "C:\Users\%USERNAME%\Downloads\AndroidLaunchers"
+echo Install the Android Launcher of your choice, Have fun!
+echo Install the Android Launcher of your choice, Have fun!
+echo Install the Android Launcher of your choice, Have fun!
+echo Install the Android Launcher of your choice, Have fun!
+echo Install the Android Launcher of your choice, Have fun!
+echo PRESS A KEY TO CLOSE THE INSTALLER
+pause
