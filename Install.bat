eb0cc72197814fa2ac193ebf0216f95b5ac63b48
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-05-17 11:22:36 +0300

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -8,6 +8,7 @@ curl https://f-droid.org/repo/fr.neamar.kiss_206.apk -o fr.neamar.kiss_206.apk
 curl https://cloclo54.datacloudmail.ru/public/get/7YHNpnRnuzvjepP2FhB626HUvf1Bzo3GPk2re4drn6SR8ykwQhBmC4nonMqVxPAYdSVRkT/no/novalauncherprime55modt-androidoyunclub.apk -o novalauncherprime55modt-androidoyunclub.apk 
 curl https://github.com/MM2-0/Kvaesitso/releases/download/v1.24.2/de.mm20.launcher2.release_2023042300.apk -o de.mm20.launcher2.release_2023042300.apk
 curl http://download2278.mediafire.com/k05ja03gjfagjDQTR9ukITJVhufO7T1sgGy2Ona971lJAq20mWvHfWZ40pZII0pWWjL9ETQ3OUaAC4kbamuY_zo5gp_ajeKWuuBacVOYwFIA5Cxbfjlcn7DvWPg7IzAWe9wK-DAjRoPU29-y1HySZpibmBrvKGoOm-LvM69R6EviYQ/ozygcd568e1amct/Trebuchet+8.1.0.22+UNIVERSAL.apk -o Trebuchet8.1.0.22UNIVERSAL.apk 
+curl https://github.com/linsui/librechair/releases/download/nightly/Librechair-debug.apk -o Librechair-debug.apk
 echo When the installer is done press Launch Please!!!
 echo When the installer is done press Launch Please!!!
 echo When the installer is done press Launch Please!!!
